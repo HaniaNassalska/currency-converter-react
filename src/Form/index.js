9b266df74a5485c6cea7ae79a1538fe50f7eee03
Hanna Nassalska <hanna.nassalska@gmail.com>
2022-06-28 17:49:13 +0200

    added 'toFixed' to the result in the Console, plnAmount -renamed to amount; empty option added to the select form

--- src/Form/index.js
+++ src/Form/index.js
@@ -2,9 +2,9 @@ import "./style.css";
 import { useState } from "react";
 
 const Form = () => {
-    
+
     const [amount, setAmount] = useState("");
-    const onInputChange = ({ target }) => setAmount(target.value); 
+    const onInputChange = ({ target }) => setAmount(target.value);
     const onFormSubmit = (event) => {
         event.preventDefault();
     };
@@ -13,28 +13,29 @@ const Form = () => {
     const [currency, setCurrency] = useState("");
     const onSelectChange = ({ target }) => setCurrency(target.value);
 
-    const plnAmount= +amount;
-   
-    const calculateResult = (plnAmount, currency) => {
+
+    const calculateResult = (amount, currency) => {
         const eurRate = 4.68;
         const usdRate = 4.22;
         const gbpRate = 5.55;
 
         switch (currency) {
             case "USD":
-                return plnAmount / usdRate;
+                return amount / usdRate;
 
             case "EUR":
-                return plnAmount / eurRate;
+                return amount / eurRate;
 
             case "GBP":
-                return plnAmount / gbpRate;
+                return amount / gbpRate;
+            default:
+                return ("0.00")
         }
-    };    
+    };
 
-const result = calculateResult(plnAmount, currency);
-console.log(result.toFixed(2))
-    
+    const result = calculateResult(amount, currency);
+    console.log(Number(result).toFixed(2))
+
     return (
         <form onSubmit={onFormSubmit}>
             <fieldset>
@@ -47,7 +48,7 @@ console.log(result.toFixed(2))
                         <span className="form__labelText">Kwota w zł*:</span>
 
                         <input value={amount} onChange={onInputChange} className="form__field"
-                            type="number" name="PLN"  min="0" step="any" required />
+                            type="number" name="PLN" min="0" step="any" required />
                     </label>
                 </p>
                 <label>
@@ -55,6 +56,7 @@ console.log(result.toFixed(2))
                 </label>
                 <select value={currency} onChange={onSelectChange} className="form__field">
 
+                    <option />
                     <option value="USD">dolar (USD)</option>
                     <option value="EUR">euro (EUR)</option>
                     <option value="GBP">funt (GBP)</option>
