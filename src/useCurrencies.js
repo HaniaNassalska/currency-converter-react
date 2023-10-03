1b7d3e2596736f525d7167a663a5293f98ac750c
Hania Nassalska <hanna.nassalska@gmail.com>
2023-10-03 23:09:00 +0200

    temporary solution related to API changes

--- src/useCurrencies.js
+++ src/useCurrencies.js
@@ -4,25 +4,31 @@ import axios from "axios";
 const useCurrencies = () => {
   const [ratesData, setRatesData] = useState({ state: "loading" });
   useEffect(() => {
-    const currenciesData = async () => {
-      try {
-        const response = await axios.get(
-          "https://api.exchangerate.host/latest?base=PLN&${+new Date()}"
-        );
-        const { date, rates } = await response.data;
+          const currenciesData = async () => {
+              try {
+                  const response = await axios.get(
+                      "https://api.exchangerate.host/latest?base=PLN&${+new Date()}"
+                  );
+                  const {date, rates} = await response.data;
 
-        setRatesData({
-          rates,
-          date,
-          state: "success",
-        });
-      } catch (error) {
-        setRatesData({ state: "error" });
-      }
-    };
+                  if (date !== undefined && rates !== undefined) {
+                      setRatesData({
+                          rates,
+                          date,
+                          state: "success",
+                      });
+                  } else {
+                      setRatesData({state: "error"});
+                  }
 
-    setTimeout(currenciesData, 1000);
-  }, []);
+              } catch (error) {
+                  setRatesData({state: "error"});
+              }
+          };
+
+          setTimeout(currenciesData, 1000);
+      },
+      []);
   return ratesData;
 };
 export default useCurrencies;
